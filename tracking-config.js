12c27bdc87cf938662c742895c6ca8afc3f44907
Albert Papp <albert.papp@onbased.com>
2026-05-16 20:27:06 +0200

    Pixel added

---
+++ tracking-config.js
@@ -0,0 +1,6 @@
+window.AGENTHEAVEN_TRACKING = {
+  bookingUrl: 'https://www.cal.eu/albert-papp-diwz8l/30min?overlayCalendar=true',
+  // Paste the numeric Meta Pixel ID here to enable homepage and thank-you conversion tracking.
+  metaPixelId: '1312428030957959',
+  googleAnalyticsId: ''
+};
