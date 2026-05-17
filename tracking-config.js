636b092c0ff30c0799f0ab2e1c02af7568a720fe
Albert Papp <albert.papp@onbased.com>
2026-05-17 10:03:12 +0200

    Install Google Analytics

--- tracking-config.js
+++ tracking-config.js
@@ -2,5 +2,5 @@ window.AGENTHEAVEN_TRACKING = {
   bookingUrl: 'https://www.cal.eu/albert-papp-diwz8l/30min?overlayCalendar=true',
   // Paste the numeric Meta Pixel ID here to enable homepage and thank-you conversion tracking.
   metaPixelId: '1312428030957959',
-  googleAnalyticsId: ''
+  googleAnalyticsId: 'G-NJ9QJRL03M'
 };
